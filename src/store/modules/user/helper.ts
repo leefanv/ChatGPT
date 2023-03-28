e4c26787c80832fa0b82d247049e127c9f482842
fan.li <fan.li@dmall.com>
2023-03-28 13:38:40 +0800

    更新名称

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >Github</a>',
+      avatar: 'https://img.seergb.com/uPic/cabinx-logo.jpeg',
+      name: 'Design 助手',
+      description: 'Dmaller 的知识百科',
     },
   }
 }
